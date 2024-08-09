b5bd8b11d5a633e3eb0a84e326de5a382f43c502
Baruch R. <azulamarillo000@gmail.com>
2024-08-09 17:23:56 -0600

    Primer endpoint: /variables

--- app.py
+++ app.py
@@ -1,12 +1,41 @@
 from flask import Flask, jsonify
+import psycopg2
 
 app = Flask(__name__)
 
-@app.route('/saluditos')
+dbhost = 'fastdb.c3.unam.mx'
+dbname = 'epi_puma_censo_inegi_2020'
+dbport = '5433'
+dbuser = 'monitor'
+dbpass = 'monitor123'
+
+@app.route('/')
 def saludo():
     return jsonify({'hola':'mundo'})
+
+@app.route('/variables')
+def fetch_variables():
+    conn_string = f'host={dbhost} dbname={dbname} user={dbuser} password={dbpass} port={dbport}'
+    conn = psycopg2.connect(conn_string)
+
+    with conn.cursor() as curs:
+        curs.execute("""with aux as (
+                        select id, name, mesh
+                            from covariable
+                        )
+                        select json_agg(aux) from aux;"""
+                     )
+
+        r = curs.fetchone()
+
+        # print(r[0][0])
+
+
+    return jsonify(r[0])
+
+    
 
 
 
 if __name__ == '__main__':
-    app.run(port=8000)
+    app.run(port=5000, debug=True)
