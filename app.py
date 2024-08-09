b71660d7189a6dc67bc753b5cf06133fb8b69971
Adrian Maldonado <am96929@gmail.com>
2024-08-09 17:25:16 -0600

    implementación de /variables/:id:

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/saluditos')
+@app.route('/')
 def saludo():
     return jsonify({'hola':'mundo'})
 
 
+@app.route('/variables/:id:')
+def variables_id():
+    q = request.args.get('q', '*')
+    offset = request.args.get('offset', None)
+    limit = request.args.get('limit', 10)
+
+    x = [
+        {'a': 1},
+        {'b' : 2}
+    ]
+
+    print(len(x))
+    return x
 
 if __name__ == '__main__':
     app.run(port=8000)
